30d999d96dff957a7aa1b886e99d45f0c0f451db
Kai <kaithomas2001@gmail.com>
2022-04-14 12:08:15 +0200

    feature: Added lowest-logging-level to DefaultChildLogger

--- src/main/kotlin/com/godbot/defaults/DefaultChildLogger.kt
+++ src/main/kotlin/com/godbot/defaults/DefaultChildLogger.kt
@@ -2,13 +2,26 @@ package com.godbot.defaults
 
 import com.godbot.Logger
 import com.godbot.LoggingLevel
+import com.godbot.lowestLoggingLevel
 
 class DefaultChildLogger(
     override val parentLogger: Logger,
     private val loggingLevel: LoggingLevel
 ) : ChildLoggerImpl(parentLogger) {
-    fun info(msg: String) = apply { super.info(msg, loggingLevel) }
-    fun warning(msg: String) = apply { super.warning(msg, loggingLevel) }
-    fun error(msg: String) = apply { super.error(msg, loggingLevel) }
-    fun fatal(msg: String) = apply { super.fatal(msg, loggingLevel) }
+    fun info(msg: String) {
+        if (loggingLevel >= lowestLoggingLevel)
+            super.info(msg, loggingLevel)
+    }
+    fun warning(msg: String) {
+        if (loggingLevel >= lowestLoggingLevel)
+            super.warning(msg, loggingLevel)
+    }
+    fun error(msg: String) {
+        if (loggingLevel >= lowestLoggingLevel)
+            super.error(msg, loggingLevel)
+    }
+    fun fatal(msg: String) {
+        if (loggingLevel >= lowestLoggingLevel)
+            super.fatal(msg, loggingLevel)
+    }
 }
