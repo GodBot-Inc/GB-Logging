d91fb2972e665b8b731eb848ffb58746ed94480f
Kai <kaithomas2001@gmail.com>
2022-04-16 16:07:50 +0200

    feature/refactor: Changed ChildLogger to follow collective logging and lowestLoggingLevel

--- src/main/kotlin/com/godbot/ChildLogger.kt
+++ src/main/kotlin/com/godbot/ChildLogger.kt
@@ -1,5 +1,8 @@
 package com.godbot
 
+import com.godbot.database.models.GroupLog
+
 interface ChildLogger: Logger {
     val parentLogger: Logger
+    val groupLog: GroupLog
 }
--- src/main/kotlin/com/godbot/defaults/ChildLoggerImpl.kt
+++ src/main/kotlin/com/godbot/defaults/ChildLoggerImpl.kt
@@ -1,27 +1,39 @@
 package com.godbot.defaults
 
-import com.godbot.ChildLogger
-import com.godbot.Logger
-import com.godbot.LoggingLevel
+import com.godbot.*
 import com.godbot.database.models.DefaultLog
+import com.godbot.database.models.GroupLog
 
 open class ChildLoggerImpl(
     override val parentLogger: Logger,
+    override val groupLog: GroupLog
 ): ChildLogger {
 
     override fun info(msg: String, lvl: LoggingLevel) {
-        println(DefaultLog(getId(), "info", lvl, msg, 1))
+        if (!collectiveLogging && lvl >= lowestLoggingLevel)
+            println(DefaultLog(getId(), "info", lvl, msg, 1))
+        else if (collectiveLogging && lvl >= lowestLoggingLevel)
+            groupLog.childLogs.add(DefaultLog(getId(), "info", lvl, msg, 1))
     }
 
     override fun warning(msg: String, lvl: LoggingLevel) {
-        println(DefaultLog(getId(), "warning", lvl, msg, 1))
+        if (!collectiveLogging && lvl >= lowestLoggingLevel)
+            println(DefaultLog(getId(), "warning", lvl, msg, 1))
+        else if (collectiveLogging && lvl >= lowestLoggingLevel)
+            groupLog.childLogs.add(DefaultLog(getId(), "warning", lvl, msg, 1))
     }
 
     override fun error(msg: String, lvl: LoggingLevel) {
-        println(DefaultLog(getId(), "error", lvl, msg, 1))
+        if (!collectiveLogging && lvl >= lowestLoggingLevel)
+            println(DefaultLog(getId(), "error", lvl, msg, 1))
+        else if (collectiveLogging && lvl >= lowestLoggingLevel)
+            groupLog.childLogs.add(DefaultLog(getId(), "error", lvl, msg, 1))
     }
 
     override fun fatal(msg: String, lvl: LoggingLevel) {
-        println(DefaultLog(getId(), "fatal", lvl, msg, 1))
+        if (!collectiveLogging && lvl >= lowestLoggingLevel)
+            println(DefaultLog(getId(), "fatal", lvl, msg, 1))
+        else if (collectiveLogging && lvl >= lowestLoggingLevel)
+            groupLog.childLogs.add(DefaultLog(getId(), "fatal", lvl, msg, 1))
     }
 }
