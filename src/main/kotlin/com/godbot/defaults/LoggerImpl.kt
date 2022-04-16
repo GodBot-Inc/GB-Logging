381332eaf6f388ec71bafd0b156f8d484cbd566c
Kai <kaithomas2001@gmail.com>
2022-04-16 16:08:46 +0200

    fix: Changed LoggerImpl to follow lowestLoggingLevel

--- src/main/kotlin/com/godbot/defaults/LoggerImpl.kt
+++ src/main/kotlin/com/godbot/defaults/LoggerImpl.kt
@@ -3,21 +3,26 @@ package com.godbot.defaults
 import com.godbot.Logger
 import com.godbot.LoggingLevel
 import com.godbot.database.models.LogImpl
+import com.godbot.lowestLoggingLevel
 
 open class LoggerImpl: Logger {
     override fun info(msg: String, lvl: LoggingLevel) {
-        println(LogImpl(getId(), "info", lvl, msg))
+        if (lvl >= lowestLoggingLevel)
+            println(LogImpl(getId(), "info", lvl, msg))
     }
 
     override fun warning(msg: String, lvl: LoggingLevel) {
-        println(LogImpl(getId(), "warning", lvl, msg))
+        if (lvl >= lowestLoggingLevel)
+            println(LogImpl(getId(), "warning", lvl, msg))
     }
 
     override fun error(msg: String, lvl: LoggingLevel) {
-        println(LogImpl(getId(), "error", lvl, msg))
+        if (lvl >= lowestLoggingLevel)
+            println(LogImpl(getId(), "error", lvl, msg))
     }
 
     override fun fatal(msg: String, lvl: LoggingLevel) {
-        println(LogImpl(getId(), "fatal", lvl, msg))
+        if (lvl >= lowestLoggingLevel)
+            println(LogImpl(getId(), "fatal", lvl, msg))
     }
 }
