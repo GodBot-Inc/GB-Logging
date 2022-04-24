54bed8462ed647e7a0a68182edd1f48d18258b87
Kai <kaithomas2001@gmail.com>
2022-04-24 16:45:02 +0200

    hotfix: ChildLogger openGroup wrong returnType

--- src/main/kotlin/com/godbot/ChildLogger.kt
+++ src/main/kotlin/com/godbot/ChildLogger.kt
@@ -5,7 +5,7 @@ import com.godbot.database.models.GroupLog
 interface ChildLogger: Logger {
     val groupLog: GroupLog
     var readyToClose: Boolean
-    fun openGroup(groupTitle: String, lvl: LoggingLevel = LoggingLevel.LOW): Logger
+    fun openGroup(groupTitle: String, lvl: LoggingLevel = LoggingLevel.LOW): ChildLogger
     fun provideClosingMessage(): String
     fun provideNonBlockingClosingMessage(): String
     fun readyToClose()
